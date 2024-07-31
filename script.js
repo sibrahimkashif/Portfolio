9cd8b592d125f2b7ceb1ce02b45840d2e8d3ca2a
Ibrahim-399 <ibrahimkashif399@gmail.com>
2024-07-31 06:37:57 +0500

    fixed some links and issues with nav on phone screen

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ function displayMenu() {
   const menuButton = document.querySelector(".menu-button");
   const nav = document.querySelector("nav");
   const logo = document.querySelector(".header-logo");
+  const header = document.querySelector("header");
+  const body = document.body;
 
   nav.classList.toggle("nav-shown");
   logo.classList.toggle("logo-hidden");
+  header.classList.toggle("header-expand");
+  body.classList.toggle("body-down");
+  menuButton.classList.toggle("menu-button-down");
 }
