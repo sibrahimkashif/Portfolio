4c30c13ab1dc4b288670ad01ac8046a536791ce1
Ibrahim-399 <ibrahimkashif399@gmail.com>
2024-07-31 17:35:09 +0500

    updated text styles and menu button style

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function displayMenu() {
-  const menuButton = document.querySelector(".menu-button");
   const menuIcon = document.querySelector(".menu-button i");
   const nav = document.querySelector("nav");
-  const logo = document.querySelector(".header-logo");
   const header = document.querySelector("header");
-  const body = document.body;
-  const navLink = document.querySelector(".nav-link");
 
   if (menuIcon.classList.contains("bx-menu")) {
     menuIcon.classList.toggle("bx-menu");
